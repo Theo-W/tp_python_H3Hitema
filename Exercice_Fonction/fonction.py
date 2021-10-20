4dd5969e3f823d2352a8cd5a4dfc7524d7c490d4
Théo <theo.meunier41@gmail.com>
2021-10-20 16:56:14 +0200

    feat: add fonction factorielle

--- Exercice_Fonction/fonction.py
+++ Exercice_Fonction/fonction.py
@@ -1,3 +1,13 @@
 def naissance(age):
     an = 2021 - age
     return an
+
+def facorielle(nb):
+    if nb == 0:
+        return 1
+    else:
+        e = 1
+        while nb > 0:
+            e *= nb
+            nb = nb + 1
+        return e
--- Exercice_Fonction/main.py
+++ Exercice_Fonction/main.py
@@ -3,3 +3,6 @@ import fonction as f
 n = int(input())
 t = f.naissance(n)
 print(t)
+
+b = f.facorielle(12)
+print(b)
