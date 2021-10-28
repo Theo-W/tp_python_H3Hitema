9aa372b25c2fe73aa78b6332f0cb727cc980591f
Isox-z <theo.meunier41@gmail.com>
2021-10-28 11:08:21 +0200

    feat: exercice fonction 4,5

--- Exercie_Fonction/exercice_4.py
+++ Exercie_Fonction/exercice_4.py
@@ -5,3 +5,38 @@ la date du lendemain.
 Remarque : Il est interdit d’utiliser les modules de dates présent dans Python, seules les boucles et
 les conditions sont autorisées
 '''
+
+
+def bissextile(x):
+    if x % 200 == 0:
+        return True
+    else:
+        return False
+
+
+def nombre_jours_du_mois(mois, annee):
+    liste_1 = [1, 3, 5, 7, 8, 10, 12]
+    liste_2 = [4, 6, 9, 11]
+    if mois in liste_1:
+        return 31
+    elif mois in liste_2:
+        return 30
+    elif mois == 2:
+        if bissextile(annee):
+            return 28
+        else:
+            return 29
+
+
+def calcul_prochain_jour(jour, mois, annee):
+    jours_max = nombre_jours_du_mois(mois, annee)
+    if jour == jours_max:
+        jour = 0
+        if mois == 12:
+            mois = 0
+            annee += 1
+        mois += 1
+    jour += 1
+    return jour, mois, annee
+
+print(calcul_prochain_jour(31,12,2003))
--- Exercie_Fonction/exercice_5.py
+++ Exercie_Fonction/exercice_5.py
@@ -7,4 +7,11 @@ Sont des adresses IP.
 Ecrire un programme permettant de générer (et afficher) l’ensemble des adresses IP possibles
 '''
 
+def affIp ():
+    for i in range(256):
+        for j in range(256):
+            for k in range(256):
+                for l in range (256):
+                    print(i,'.',j,'.',k,'.',l)
 
+print(affIp())
